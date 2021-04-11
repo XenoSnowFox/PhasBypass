9dccf6443431cd599af1fdeee261391252bc610c
AshleyHolton <ashleystaggs@gmail.com>
2021-04-11 16:10:38 +0100

    Fixed for 6429688
    Needs to have the unicode rubbish pruned. Not sure how to do that yet.

--- user/main.cpp
+++ user/main.cpp
@@ -15,73 +15,64 @@
 
 using namespace app;
 
-// Set the name of your log file here
-extern const LPCWSTR LOG_FILE = L"bypass-log.txt";
+extern const LPCWSTR LOG_FILE = L"il2cpp-log.txt";
 
 const std::string NotMelonLoader = "totally_not_melon_loader";
-
 String* not_melon_loader;
 
 void DoNothingMethod(MethodInfo* method)
 {
-	// nothing
-	LogWrite("Hit DoNothingMethod\n");
+	il2cppi_log_write("DoNothingCalled");
 }
 
 bool File_Exists_Hook(String* str, MethodInfo* method)
 {
-	//the only dll it looks for using File.Exists() are version.dll, winhttp.dll and winmm.dll
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> wideToNarrow;
 	std::string skey = wideToNarrow.to_bytes(std::wstring((const wchar_t*)
 		(&((Il2CppString*)str)->chars), ((Il2CppString*)str)->length));
 
-	//printf("Searching for %s\n", skey.c_str());
-
-	char p[500];
-
-	sprintf_s(p, "Searching for file %s", skey.c_str());
-
-	LogWrite(p);
-
-	if (skey.find("dll") != std::string::npos || skey.find(NotMelonLoader) != std::string::npos)
+	if(skey.find("dll") != std::string::npos || skey.find(NotMelonLoader) != std::string::npos)
 	{
-		LogWrite(" blocked\n");
 		return false;
 	}
 
-	LogWrite("\n");
+	il2cppi_log_write("FileHooked\n");
+
 	return File_Exists(str, method);
 }
 
 bool Directory_Exists_Hook(String* str, MethodInfo* method)
 {
-
-	//the only dll it looks for using File.Exists() are version.dll, winhttp.dll and winmm.dll
 	std::wstring_convert<std::codecvt_utf8<wchar_t>> wideToNarrow;
 	std::string skey = wideToNarrow.to_bytes(std::wstring((const wchar_t*)
 		(&((Il2CppString*)str)->chars), ((Il2CppString*)str)->length));
 
-	//printf("Searching for %s\n", skey.c_str());
-
-	char p[500];
-
-	sprintf_s(p, "Searching for directory %s", skey.c_str());
-
-	LogWrite(p);
-
-	if (skey.find("MelonLoader") != std::string::npos || skey.find(NotMelonLoader) != std::string::npos)
+	if(skey.find("MelonLoader") != std::string::npos || skey.find(NotMelonLoader) != std::string::npos)
 	{
-		LogWrite(" blocked\n");
 		return false;
 	}
 
-	LogWrite("\n");
+	il2cppi_log_write("DirectoryHooked\n");
+
 	return Directory_Exists(str, method);
 }
 
+bool String_Contains_Hook(String* str, String* str2, MethodInfo* method)
+{
+	std::wstring_convert<std::codecvt_utf8<wchar_t>> wideToNarrow;
+	std::string skey = wideToNarrow.to_bytes(std::wstring((const wchar_t*)
+		(&((Il2CppString*)str)->chars), ((Il2CppString*)str)->length));
 
-//    IntPtr \u0923\u0926\u0922\u0920\u0922\u091B\u0922\u091F\u0927\u0929\u0922(String)
-// 18123e620
+	il2cppi_log_write(skey);
+
+	if(skey.find("MelonLoader") != std::string::npos || skey.find(NotMelonLoader) != std::string::npos)
+	{
+		return false;
+	}
+
+	return String_Contains(str, str2, method);
+}
+
 void* TryGetModuleHandleHook(String* str, MethodInfo* method)
 {
 
@@ -89,95 +80,49 @@ void* TryGetModuleHandleHook(String* str, MethodInfo* method)
 	std::string skey = wideToNarrow.to_bytes(std::wstring((const wchar_t*)
 		(&((Il2CppString*)str)->chars), ((Il2CppString*)str)->length));
 
-	char buf[500]{};
+	il2cppi_log_write(skey);
 
-	sprintf_s(buf, "blocked module handle of %s\n", skey.c_str());
-	LogWrite(buf);
 	return nullptr;
 }
 
-
-// String \u091C\u0925\u0926\u0920\u0923\u091C\u0920\u091B\u091D\u091B\u091B(Byte[], Boolean)
-// in __199 class
-// 1816b48c0
 String* GetMelonLoaderSearchStrings(Byte__Array* theArray, bool b, MethodInfo* method)
 {
-	/*String* res = __202_____________29(theArray, b, method);
-	std::wstring_convert<std::codecvt_utf8<wchar_t>> wideToNarrow;
-	std::string skey = wideToNarrow.to_bytes(std::wstring((const wchar_t*)
-		(&((Il2CppString*)res)->chars), ((Il2CppString*)res)->length));
-
-	char buf[500]{};
-
-	sprintf_s(buf, "ComparString called, res is %s\n", skey.c_str());
-
-	LogWrite(buf);*/
-
-	//log muted for this because it is called A LOT
-
 	return not_melon_loader;
 }
 
-// Custom injected code entry point
+void CheckProcessesForModsFunc(MethodInfo* method)
+{
+	il2cppi_log_write("CheckProcessesForMods");
+}
+
+void CheckForModsFunc(MethodInfo* method)
+{
+	il2cppi_log_write("DoNothingCalled");
+}
+
 void Run()
 {
-
 	NewConsole();
 
-	LogWrite("Starting bypass\n");
-
+	il2cppi_log_write("Starting Hooks\n");
 	not_melon_loader = (String*)il2cpp_string_new(NotMelonLoader.c_str());
-
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
-	// This method checks if the following apps are running.
-	// It gets all the system's process and compares their names with the ones below, if any matches it calls Application.Quit()
-	// - cheatengine
-	// - sharpmonoinjector
-	// - fiddler
-	// - dnspy
-	// - ollydbd
-	// NOTE: this seems to be buggy because before finding this I got Phasmophobia running with Cheat Engine in the background.
-	DetourAttach(&(PVOID&)__102____________, DoNothingMethod);
-
-	// This method checks for the following files in the games data directory, on the first one found, it deletes it and calls Application.Quit()
-	// - modconfig.cfg
-	// - sharpmonoinjector.dll
-	// - coolphobialauncher.exe
-	// - loader.exe
-	// - launcher.exe
-	DetourAttach(&(PVOID&)__102_____________1, DoNothingMethod);
-
-
-	// This method just checks your money and clamps it to 250000 if it's above it
-	// The Phasmophobia developer removed this check!
-	//DetourAttach(&(PVOID&)__105____________, DoNothingMethod);
-
-
-	// this method calls GetModuleHandle, it is only called by the game's code and is used to detect loaded dlls
-	DetourAttach(&(PVOID&)__101_____________7, TryGetModuleHandleHook);
-
-
-	// I haven't been able to find where exactly it looks for the dll files, so I'm just hooking File.Exists(), and returning false manually when it looks for a dll
-
+	DetourAttach(&(PVOID&)CheckForGetModuleHandle, TryGetModuleHandleHook);
+	DetourAttach(&(PVOID&)CheckProcessesForMods, CheckProcessesForModsFunc);
+	DetourAttach(&(PVOID&)CheckForMods, CheckForModsFunc);
+	DetourAttach(&(PVOID&)__101____________5, DoNothingMethod);
+	DetourAttach(&(PVOID&)__101____________2, DoNothingMethod);
+	DetourAttach(&(PVOID&)__101____________6, DoNothingMethod);
+	DetourAttach(&(PVOID&)__101____________4, DoNothingMethod);
 	DetourAttach(&(PVOID&)File_Exists, File_Exists_Hook);
-
-	// same for Directory.Exists()
 	DetourAttach(&(PVOID&)Directory_Exists, Directory_Exists_Hook);
+	DetourAttach(&(PVOID&)String_Contains, String_Contains_Hook);
+	LONG lError = DetourTransactionCommit();
+	if(lError != NO_ERROR)
+	{
+		il2cppi_log_write("Hook Failed");
+	}
 
-
-	// replaces the text it checks for
-
-
-	DetourAttach(&(PVOID&)__199_____________15, GetMelonLoaderSearchStrings);
-
-	// hooks quit for debugging
-	/*DetourAttach(&(PVOID&)Application_Quit_1, DoNothingMethod);
-	DetourAttach(&(PVOID&)Application_Quit, DoNothingMethod);
-	DetourAttach(&(PVOID&)Utils_1_ForceCrash, DoNothingMethod);*/
-
-
-	DetourTransactionCommit();
-	LogWrite("Bypass hooked\n");
-
+	il2cppi_log_write("Bypass hooked\n");
 }
